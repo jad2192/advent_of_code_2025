c1f09546443f5d6164f483c1e25cb5d36188cec4
James Diotte <james@findanomaly.com>
2025-12-01 12:36:33 -0500

    Simpler Solution

--- aoc2025/day01.py
+++ aoc2025/day01.py
@@ -1,44 +1,28 @@
 from aoc2025 import load_input
 
+DXN = {"L": -1, "R": 1}
 
-def count_zeros_terminating(instructions: list[str]) -> int:
-    count, pos = 0, 50
+
+def count_zeros(instructions: list[str]) -> tuple[int, int]:
+    count_end, count_pass, pos = 0, 0, 50
     for step in instructions:
-        dxn, mag = step[0], int(step[1:])
-        if dxn == "R":
-            pos = (pos + mag) % 100
-        else:
-            pos = (pos - mag) % 100
-        count += pos == 0
-    return count
-
-
-def count_zeros_passing(instructions: list[str]) -> int:
-    count, pos = 0, 50
-    for step in instructions:
-        dxn, mag = step[0], int(step[1:])
-        rotations = abs(mag) // 100
-        mag = mag % 100
-        if dxn == "R":
-            pos = pos + mag
-            count += rotations + (pos - mag > 0) * (pos >= 100)
-        else:
-            pos = pos - mag
-            count += rotations + (pos + mag > 0) * (pos <= 0)
+        dxn, mag = DXN[step[0]], int(step[1:])
+        rotations, mag = abs(mag) // 100, mag % 100
+        pos = pos + (dxn * mag)
+        count_end += pos % 100 == 0
+        count_pass += rotations + (pos * (pos - dxn * mag) <= 0) + (pos >= 100) - ((pos - dxn * mag) % 100 == 0)
         pos = pos % 100
-    return count
+    return count_end, count_pass
 
 
 # Test
 test_instructions = load_input(1, "test").split("\n")
-test_res = count_zeros_terminating(test_instructions)
+test_res, test_res2 = count_zeros(test_instructions)
 assert test_res == 3
-test_res2 = count_zeros_passing(test_instructions)
 assert test_res2 == 6
 
 # Solutions
 instructions = load_input(1, "main").split("\n")
-part_1 = count_zeros_terminating(instructions)
+part_1, part_2 = count_zeros(instructions)
 print(f"Part 1: {part_1}")
-part_2 = count_zeros_passing(instructions)
 print(f"Part 2: {part_2}")
