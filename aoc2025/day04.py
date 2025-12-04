216610222872747cb1b8918cf54ce469cf9cb5b0
James Diotte <james@findanomaly.com>
2025-12-04 09:47:07 -0500

    Little more efficient

--- aoc2025/day04.py
+++ aoc2025/day04.py
@@ -6,21 +6,21 @@ class Diagram:
         self.num_rows = len(diagram_list)
         self.num_cols = len(diagram_list[0])
         self.grid: dict[complex, str] = {
-            complex(col, row): chr for row in range(self.num_rows) for col, chr in enumerate(diagram_list[row])
+            complex(col, row): chr
+            for row in range(self.num_rows)
+            for col, chr in enumerate(diagram_list[row])
+            if chr == "@"
         }
         self.accessible_roll_coords = set()
 
     def get_neighbors(self, z: complex) -> list[complex]:
         dxns: list[complex] = [1, -1, 1j, -1j, 1 + 1j, 1 - 1j, -1 + 1j, -1 - 1j]
-        return [z + w for w in dxns if (0 <= (z + w).imag < self.num_rows) and (0 <= (z + w).real < self.num_cols)]
+        return [z + w for w in dxns if (z + w) in self.grid]
 
     def get_accessible_rolls(self) -> int:
         accessible_rolls = 0
         for coord in self.grid:
-            if self.grid[coord] != "@":
-                continue
-            neighbor_rolls = [self.grid[z] for z in self.get_neighbors(coord) if self.grid[z] == "@"]
-            if len(neighbor_rolls) < 4:
+            if len(self.get_neighbors(coord)) < 4:
                 accessible_rolls += 1
                 self.accessible_roll_coords.add(coord)
         return accessible_rolls
@@ -29,8 +29,7 @@ class Diagram:
         removed_rolls = 0
         while self.get_accessible_rolls() > 0:
             removed_rolls += len(self.accessible_roll_coords)
-            for coord in self.accessible_roll_coords:
-                self.grid[coord] = "."
+            self.grid = {z: chr for z, chr in self.grid.items() if z not in self.accessible_roll_coords}
             self.accessible_roll_coords = set()
         return removed_rolls
 
