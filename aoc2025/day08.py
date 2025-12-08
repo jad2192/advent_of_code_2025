29d6c29268d0b4276ab99d7c85fdadb620373ee6
James Diotte <james@findanomaly.com>
2025-12-08 11:08:54 -0500

    More Efficient

--- aoc2025/day08.py
+++ aoc2025/day08.py
@@ -11,54 +11,33 @@ class JunctionBoxArray:
         }
         self.sorted_dists = sorted(self.distances.keys(), key=lambda k: self.distances[k])
         self.circuits: dict[int, set[int]] = {k: {k} for k in range(len(self.box_locs))}
-        self.connected_components: list[set[int]] = []
 
     def l2_dist(self, box1_ix: int, box2_ix: int) -> int:
         return sum((x1 - x2) ** 2 for x1, x2 in zip(self.box_locs[box1_ix], self.box_locs[box2_ix]))
 
-    def connect_boxes(self, n_connections: int, start_ix: int):
-        for ix_pair in self.sorted_dists[start_ix : start_ix + n_connections]:
-            self.circuits[ix_pair[0]].add(ix_pair[1])
-            self.circuits[ix_pair[1]].add(ix_pair[0])
-
     def merge_circuits(self, n_connections: int, start_ix: int = 0):
-        self.connect_boxes(n_connections, start_ix)
-        overall_seen = set()
-        connected_components = []
-        for k in self.circuits:
-            if k in overall_seen:
-                continue
-            cur_seen, cur_comp = set(), {k}
-            dfs_stack = [k]
-            while dfs_stack:
-                cur_box = dfs_stack.pop()
-                cur_seen.add(cur_box)
-                updates = self.circuits[cur_box].difference(cur_seen)
-                dfs_stack.extend(updates)
-                cur_comp.update(updates)
-            overall_seen.update(cur_comp)
-            connected_components.append(cur_comp)
-        self.connected_components = connected_components
-        for comp in self.connected_components:
-            for circ in comp:
-                self.circuits[circ] = comp
+        for box1, box2 in self.sorted_dists[start_ix : start_ix + n_connections]:
+            self.circuits[box1].update(self.circuits[box2])
+            for box in self.circuits[box2]:
+                self.circuits[box] = self.circuits[box1]
 
     def largest_3_circuit_output(self, n_connections: int) -> int:
         self.merge_circuits(n_connections)
-        self.connected_components.sort(key=len, reverse=True)
-        con_comp = self.connected_components[:3]
-        return len(con_comp[0]) * len(con_comp[1]) * len(con_comp[2])
+        connected_components = sorted(
+            set(tuple(sorted(circ)) for circ in self.circuits.values()), key=len, reverse=True
+        )
+        return len(connected_components[0]) * len(connected_components[1]) * len(connected_components[2])
 
     def largest_extension_length(self, start: int) -> int:
-        ix, delta, ceiling = start, 10 * start, None
+        ix, delta, ceiling = start, start, None
         found = False
         while not found:
             prev_circuits = copy.deepcopy(self.circuits)
             self.merge_circuits(n_connections=delta, start_ix=ix)
-            if len(self.connected_components) > 1:
+            if len(self.circuits[0]) < len(self.box_locs):
                 ix += delta
                 if ceiling is None:
-                    delta *= 2
+                    pass
                 else:
                     delta = max(1, int(ceiling - ix) // 2)
             else:
